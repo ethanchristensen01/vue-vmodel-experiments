72b79aa4c6cba29f4f51de5dfcabf562238c1414
Ethan Christensen <ethanchristensen01@proton.me>
2026-02-11 01:02:31 -0700

    Add mirror and equality debug

--- interview-vue/src/views/MainPage.vue
+++ interview-vue/src/views/MainPage.vue
@@ -1,13 +1,34 @@
 <template>
-  <fieldset>
-    <TodoList
-      v-model:model-value="tasks"
-      @update:model-value="onUpdate"
-    />
-  </fieldset>
-  <div v-if="allDone">
-    All done!
-  </div>
+  <section>
+    <h2>Todos</h2>
+    <fieldset>
+      <TodoList
+        v-model:model-value="tasks"
+        @update:model-value="onUpdate"
+      />
+    </fieldset>
+    <div v-if="allDone">
+      All done!
+    </div>
+  </section>
+  <section>
+    <details>
+      <summary>Debug</summary>
+      <div>
+        <h3>Copy of Todos</h3>
+        <fieldset>
+          <TodoList
+            v-model="tasksMirror"
+          />
+        </fieldset>
+      </div>
+      <output>
+        <code>
+          tasks.value === tasksMirror.value: {{ tasks === tasksMirror }}
+        </code>
+      </output>
+    </details>
+  </section>
 </template>
 
 <script setup lang="ts">
@@ -31,6 +52,7 @@ const allDone = computed(() => {
   return tasks.value.every(t => t.isAllComplete())
 })
 
+
 watch(() => tasks, v => console.log('watch task', v.value))
 watch(() => tasks, v => console.log('watch task deep', v.value), { deep: true })
 
@@ -38,8 +60,22 @@ function onUpdate (value?: Task[]) {
   console.log('v-model update', value)
 }
 
+const tasksMirror = ref(tasks.value)
 </script>
 
 <style scoped>
+* {
+  box-sizing: border-box;
+}
 
+fieldset {
+  width: fit-content;
+  margin-bottom: 1em;
+}
+
+output {
+  background-color: #171717;
+  padding: 0.5em;
+  min-height: 6em;
+}
 </style>
